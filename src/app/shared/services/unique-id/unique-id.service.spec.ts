49005495ebff9b02cf4be1b4a82d9d1239dcc935
Rodrigo Kobayashi <rodrigo_kobayashi@outlook.com>
2021-04-21 20:01:16 -0300

    feature(Testes):Continuação Estudos

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,9 +1,38 @@
-import { UniqueServiceId } from './unique-id.service';
+import { UniqueIdService } from './unique-id.service';
 
-describe(UniqueServiceId.name, () => {
-  it(`#${UniqueServiceId.prototype.genereteUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
-    const service = new UniqueServiceId();
+describe(UniqueIdService.name, () => {
+
+  let service: UniqueIdService = null;
+  beforeEach(() => {
+    service = new UniqueIdService();
+  });
+
+  it(`#${UniqueIdService.prototype.genereteUniqueIdWithPrefix.name}
+    should generate id when called with prefix`, () => {
     const id = service.genereteUniqueIdWithPrefix('app');
     expect(id.startsWith('app-')).toBeTrue();
+  });
+
+  it(`#${UniqueIdService.prototype.genereteUniqueIdWithPrefix.name}
+    should not generate duplicate IDs when called multiple times`, () => {
+    const ids = new Set();
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.genereteUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
+  });
+
+  it(`#${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name}
+    should return the number of generated Ids when called`, () => {
+    service.genereteUniqueIdWithPrefix('app');
+    service.genereteUniqueIdWithPrefix('app');
+    expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
+  });
+
+  it(`#${UniqueIdService.prototype.genereteUniqueIdWithPrefix.name}
+    should throw when called with empty`, () => {
+    const emptyValues = [null, undefined, ''];
+    emptyValues.forEach(emptyValues => expect(
+      () => service.genereteUniqueIdWithPrefix(emptyValues)).toThrow());
   });
 });
--- src/app/shared/services/unique-id/unique-id.service.ts
+++ src/app/shared/services/unique-id/unique-id.service.ts
@@ -4,7 +4,7 @@ import { v4 as uuidv4 } from 'uuid';
 @Injectable({
   providedIn: 'root'
 })
-export class UniqueServiceId {
+export class UniqueIdService {
 
   private numberOfGeneratedIds = 0;
 
